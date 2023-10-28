e0bb14123f4eb87dddd58dc922598f53e5e6ea96
Kaizhe Hu <hkz22@mails.ttsinghua.edu.cn>
2023-10-28 16:03:33 +0800

    add dino_sd baseline

--- baselines/sd_dino/eval.py
+++ baselines/sd_dino/eval.py
@@ -1,14 +1,13 @@
-import re
 import os
+import re
 import cv2
-import torch
 import pickle
 import numpy as np
 from PIL import Image
 from tqdm import tqdm
+from get_corr import get_corr_pairs
+from extractor_sd import load_model
 import matplotlib.pyplot as plt
-from optimize import optimize_prompt, run_image_with_tokens_cropped, find_max_pixel_value, load_ldm
-
 
 def nearest_distance_to_mask_contour(mask, x, y):
     # Convert the boolean mask to an 8-bit image
@@ -36,59 +35,6 @@ def nearest_distance_to_mask_contour(mask, x, y):
     # normalize the distance with the diagonal length of the mask
     diag_len = np.sqrt(mask.shape[0]**2 + mask.shape[1]**2)
     return abs(min_distance) / diag_len
-
-
-def get_cor_pairs(ldm, src_image, trg_image, src_points, img_size):
-    """
-    src_image, trg_image: relative path of src and trg images
-    src_points: resized affordance points in src_image
-    average_pts: average before correspondance or not
-    -----
-    return: correspondance maps of each src_point and each target_point
-    """
-    trg_points = []
-    layers = [5,6,7,8]
-    with Image.open(src_image) as img:
-        src_w, src_h = img.size
-        src_image = img.resize((img_size, img_size), Image.BILINEAR).convert('RGB')
-        src_tensor = torch.Tensor(np.array(src_image).transpose(2, 0, 1)) / 255.0
-        src_x_scale, src_y_scale = img_size / src_w, img_size / src_h
-    with Image.open(trg_image) as img:
-        trg_w, trg_h = img.size
-        trg_image = img.resize((img_size, img_size), Image.BILINEAR).convert('RGB')
-        trg_tensor = torch.Tensor(np.array(trg_image).transpose(2, 0, 1)) / 255.0
-        trg_x_scale, trg_y_scale = img_size / trg_w, img_size / trg_h
-    
-    src_points = [torch.Tensor([int(np.round(x * src_x_scale)), int(np.round(y * src_y_scale))]) for (x, y) in src_points]
-    all_contexts = []
-    for src_point in src_points:
-        contexts = []
-        for _ in range(5):
-            context = optimize_prompt(ldm, src_tensor, src_point/img_size, num_steps=129, device='cuda:0', layers=layers, lr = 0.0023755632081200314, upsample_res=img_size, noise_level=-8, sigma = 27.97853316316864, flip_prob=0.0, crop_percent=93.16549294381423)
-            contexts.append(context)
-        all_contexts.append(torch.stack(contexts))
-        
-        all_maps = []
-        for context in contexts:
-            maps = []
-            attn_maps, _ = run_image_with_tokens_cropped(ldm, trg_tensor, context, index=0, upsample_res = img_size, noise_level=-8, layers=layers, device='cuda:0', crop_percent=93.16549294381423, num_iterations=20, image_mask = None)
-            for k in range(attn_maps.shape[0]):
-                avg = torch.mean(attn_maps[k], dim=0, keepdim=True)
-                maps.append(avg)
-            maps = torch.stack(maps, dim=0)
-            all_maps.append(maps)
-        all_maps = torch.stack(all_maps, dim=0)
-        all_maps = torch.mean(all_maps, dim=0)
-        all_maps = torch.nn.Softmax(dim=-1)(all_maps.reshape(len(layers), img_size*img_size))
-        all_maps = all_maps.reshape(len(layers), img_size, img_size)
-        
-        all_maps = torch.mean(all_maps, dim=0)
-        trg_points.append(find_max_pixel_value(all_maps, img_size = img_size).cpu().numpy())
-    
-    
-    trg_points = [[int(np.round(x / trg_x_scale)), int(np.round(y / trg_y_scale))] for (x, y) in trg_points]
-    
-    return trg_points
 
 def analyze_dists(total_dists):
     all_dists = []
@@ -121,8 +67,7 @@ def plot_img_pairs(imglist, points, trg_mask, save_name='corr.png', fig_size=3, 
     plt.savefig(save_name)
     plt.close()
     
-    
-def dataset_walkthrough(ldm, img_size, exp_name, visualize=False, average_pts=True):
+def dataset_walkthrough(model, aug, exp_name, visualize=False, average_pts=True):
     base_dir, gt_dir = 'eval_all/egocentric', 'eval_all/GT'
     total_dists = {}
     for action in (pbar_a := tqdm(os.listdir(base_dir))):
@@ -144,35 +89,30 @@ def dataset_walkthrough(ldm, img_size, exp_name, visualize=False, average_pts=Tr
                             trg_mask = np.array(img) > 122
                     elif file.endswith('.txt'):
                         src_image = os.path.join(instance_path, file).replace('txt', 'png')
+                        src_object = file.split('_')[0]
                         with open(os.path.join(instance_path, file), 'r') as f:
                             lines = f.readlines()
                             src_points = [list(map(float, line.rstrip().split(','))) for line in lines if re.match(r'^\d+.\d+,\d+.\d+$', line.rstrip())]
                             if average_pts:
                                 src_points = [np.mean(np.array(src_points), axis=0).astype(np.int32)]
-                trg_points = get_cor_pairs(ldm, src_image, trg_image, src_points, img_size)
+                src_prompt, trg_prompt = [f'a photo of a {src_object}', f'a photo of {trg_object}']
+                trg_points = get_corr_pairs(model, aug, src_image, trg_image, src_points, src_prompt, trg_prompt)
                 trg_point = np.mean(trg_points, axis=0)
                 trg_dist = nearest_distance_to_mask_contour(trg_mask, trg_point[0], trg_point[1])
                 total_dists[action][trg_object].append(trg_dist)
                 if visualize:
                     imglist = [Image.open(file).convert('RGB') for file in [src_image, trg_image]]
-                    os.makedirs(f'results/baselines/ldm_sc/{exp_name}/{action}/{trg_object}', exist_ok=True)
-                    plot_img_pairs(imglist, [src_points, trg_points], trg_mask, f'results/baselines/ldm_sc/{exp_name}/{action}/{trg_object}/{instance}_{trg_dist:.2f}.png')
+                    os.makedirs(f'results/baselines/sd_dino/{exp_name}/{action}/{trg_object}', exist_ok=True)
+                    plot_img_pairs(imglist, [src_points, trg_points], trg_mask, f'results/baselines/sd_dino/{exp_name}/{action}/{trg_object}/{instance}_{trg_dist:.2f}.png')
     return total_dists
 
-
 if __name__ == '__main__':
-    img_size = 512
-    visualize, average_pts = True, False
-    exp_name = 'no_avg_pts'
-    ldm = load_ldm('cuda:0', 'CompVis/stable-diffusion-v1-4')
-    # src_image = 'eval_all/egocentric/drag/suitcase/suitcase_000529/cabinet_01.png'
-    # trg_image = 'eval_all/egocentric/drag/suitcase/suitcase_000529/suitcase_000529.jpg'
-    # with open('eval_all/egocentric/drag/suitcase/suitcase_000529/cabinet_01.txt', 'r') as f:
-    #     lines = f.readlines()
-    #     src_points = [list(map(float, line.rstrip().split(','))) for line in lines if re.match(r'^\d+.\d+,\d+.\d+$', line.rstrip())]
-    # src_points = [np.mean(src_points, axis=0).astype(np.int32)]
-    total_dists = dataset_walkthrough(ldm, img_size, exp_name, visualize, average_pts)
-    with open(f'results/baselines/ldm_sc/{exp_name}/total_dists.pkl', 'wb') as f:
-        pickle.dump(total_dists, f)
+    visualize = True
+    exp_name, average_pts = 'avg_pts', True
+    model, aug = load_model(diffusion_ver='v1-5', image_size=960, num_timesteps=100, block_indices=(2,5,8,11))
+    total_dists = dataset_walkthrough(model, aug, exp_name, visualize, average_pts)
     analyze_dists(total_dists)
+    with open(f'results/baselines/sd_dino/{exp_name}/total_dists.pkl', 'wb') as f:
+        pickle.dump(total_dists, f)
+    
     
